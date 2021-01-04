37ddc750874e11b9387d77ca9512ed139e05af54
hg-pyun <phg2491@gmail.com>
2021-01-05 01:33:48 +0900

    add: file-loader

--- webpack.config.js
+++ webpack.config.js
@@ -44,6 +44,14 @@ function makeConfig(mode) {
           use: 'ts-loader',
           exclude: /node_modules/,
         },
+        {
+          test: /\.(png|jpe?g|gif)$/i,
+          use: [
+            {
+              loader: 'file-loader',
+            },
+          ],
+        },
       ],
     },
     resolve: {
